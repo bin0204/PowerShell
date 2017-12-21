82305be87f38fae3a3f8353858339d52f83c8913
Seopin Nam <seobin1026@gmail.com>
2017-12-20 16:21:53 -1000

    add comments

--- ipAddress.ps1
+++ ipAddress.ps1
@@ -1,16 +1,20 @@
+#get DNS information
 $allInfo = [System.Net.Dns]::GetHostByName(($env:computerName))
+#get AddressList and then convert it to string
 $ipAddress = $allInfo.AddressList[0].IPAddressToString
 
-$ipElement = $ipAddress.Split(".")
+#$ipElement = $ipAddress.Split(".")
 
-
+#make new array
 $newArray = @()
 
+#split ipaddress by dot then append into newArray
 foreach ($i in $ipAddress){
 	$a = $i.Split(".")
-	$newArray += $a	
+	$newArray += $a
 }
 
+#when second element is equal to 255, then print out sentence
 if ($newArray[2] -eq 255) {
 	'The third element of ipAddress is 255'
 }
